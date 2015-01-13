a99d072c46c576ee03729720f2c6848a1b79aafd
Nicholas Eduardo <nicholasess@gmail.com>
2015-01-12 23:15:07 -0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-	$('#mesa_desc').text("Mesa "+localStorage.getItem('mesa'));
+  $('#mesa_desc').text("Mesa "+localStorage.getItem('mesa'));
+  window.urlServer = "";
+  if(localStorage.getItem('ip')){
+    window.urlServer = 'http://'+localStorage.getItem('ip')+'/admin/API/baixa/';  
+  }else{
+    location.replace("add_server.html?msg=1");
+  }
 });
+
